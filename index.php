6869b3f054811a984176e5dd31983ab7c02f8b85
ChloeLedru <62375513+ChloeLedru@users.noreply.github.com>
2020-06-26 10:29:40 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+	<head>
+		<meta charset="UTF-8">
+		<title> ÉVALUATION PHP CHLOÉ</title>
+	</head>
+
+	<body>
+		<form name="formulaire" action="/ma-page-de-traitement" method="post">
+    	<div>
+        	<label for="name">Nom et prénom :</label>
+        	<input type="text"  name="clname">
+    	</div>
+
+    	<div>
+       	 <label for="city">Ville :</label>
+        	<input type="city"  name="clcity">
+   	 	</div>
+
+		<div>
+       	 <label for="phone">Téléphone :</label>
+        	<input type="phone"  name="clphone">
+   	 	</div>
+
+    	<div>
+       	 <label for="mail">Adresse e-mail :</label>
+        	<input type="email"  name="clmail">
+   	 	</div>
+   	 
+		</form>
+
+
+<?php
+// Formulaire
+?>
+
+	</body>
+</html>
